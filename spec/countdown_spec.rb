0401d707c77b218a74492e65797ea14f682df8b3
Arthur Song <arth3rs0ng@gmail.com>
2019-04-03 04:46:27 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -9,7 +9,7 @@ describe 'LOOPS' do
 
     it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
       binding.pry
-      expect { countdown(10) }.to output(countdown_output).to_stdout
+      expect(countdown(10)).to output(countdown_output).to_stdout
       binding.pry
     end
 
